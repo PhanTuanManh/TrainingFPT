a080b10c8ed008993af3ad639848499fe039cab2
ThanhTrieu <nguyenthanhtrieu90@gmail.com>
2024-03-12 12:12:24 +0700

    update code

---
+++ TrainingFPT/Models/Queries/CategoryQuery.cs
@@ -0,0 +1,17 @@
+﻿namespace TrainingFPT.Models.Queries
+{
+    public class CategoryQuery
+    {
+        // viet method insert category
+        public int InsertItemCategory(
+            string nameCategory,
+            string description,
+            string image,
+            string status
+        ) {
+            int lastIdInsert = 0; // id cua category vua dc them moi
+
+            return lastIdInsert;
+        }
+    }
+}
